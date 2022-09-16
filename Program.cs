f429525b7787f0c234234f371a28d0b07f9ac070
Rafael <faelmota7@gmail.com>
2022-09-16 20:42:00 -0300

    TESTE

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿string nome;
+
+// "F2" substitui o nome da variavel em qualquer parte do programa
+
+decimal numero1 = 7;
+decimal numero2 = 3;
+
+decimal soma;
+soma = numero1 + numero2;
+
+decimal quociente = numero1 / numero2;
+decimal resto = numero1 % numero2;
+
+
+nome = "Maria";
+nome = "Teresa".ToUpper();
+
+
+string sobrenome = "da Silva";
+
+Console.WriteLine(nome);
+Console.WriteLine(sobrenome);
+Console.WriteLine(soma);
+Console.WriteLine(quociente);
+Console.WriteLine(resto);
