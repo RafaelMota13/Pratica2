a68d25a0f23c2282c93078c17bd484eba047c650
Rafael <faelmota7@gmail.com>
2022-09-16 21:46:34 -0300

    TESTE2

--- Program.cs
+++ Program.cs
@@ -23,3 +23,43 @@ Console.WriteLine(sobrenome);
 Console.WriteLine(soma);
 Console.WriteLine(quociente);
 Console.WriteLine(resto);
+
+
+
+string numeroDigitado = Console.ReadLine()!;
+
+ int a = Convert.ToInt32(numeroDigitado);
+
+int idade = Convert.ToInt32(Console.ReadLine());
+double alturaMetros = Convert.ToDouble(Console.ReadLine());
+decimal salario = Convert.ToDecimal(Console.ReadLine());
+
+int numFilhos = 0;
+numFilhos = 1;
+numFilhos = 2;
+
+
+const double percentualFGTS = 8/100;
+
+decimal salarioLiquido = 1000 * (1 - percentualFGTS);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
